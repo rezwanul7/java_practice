af1aa71075dc8bca8f26653fdb25c29149888eec
Rezwanul Islam <rezwanul7@gmail.com>
2021-08-02 16:37:25 +0600

    add oop shadow class

---
+++ src/com/rezwanul7/oop/classes/inner/shadow/ShadowTest.java
@@ -0,0 +1,26 @@
+package com.rezwanul7.oop.classes.inner.shadow;
+
+public class ShadowTest {
+
+    public int x = 0;
+    public int y = 0;
+
+    class FirstLevel {
+
+        public int x = 1;
+
+        void methodInFirstLevel(int x) {
+            System.out.println("x = " + x);
+            System.out.println("this.x = " + this.x);
+            System.out.println("ShadowTest.this.x = " + ShadowTest.this.x);
+//            ShadowTest.this.y;
+        }
+    }
+
+    public static void main(String... args) {
+        ShadowTest st = new ShadowTest();
+        ShadowTest.FirstLevel fl = st.new FirstLevel();
+        fl.methodInFirstLevel(23);
+    }
+}
+
